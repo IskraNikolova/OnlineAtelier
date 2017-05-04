44c9cd6e65c3e2dcf7552c449b22741e173dd28f
Isibel <air_iskra@yahoo.com>
2017-05-04 14:31:14 +0300

    add admin notifications with signalr

---
+++ Source/Web/OnlineAtelier.Web/Areas/Admin/Controllers/NotificationsController.cs
@@ -0,0 +1,26 @@
+﻿namespace OnlineAtelier.Web.Areas.Admin.Controllers
+{
+
+    using System.Web.Mvc;
+    using Microsoft.AspNet.SignalR;
+    using Hubs;
+
+    public class NotificationsController : BaseAdminController
+    {
+        [HttpGet]
+        [Route("Notifications")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateInput(false)]
+        [Route("Notifications")]
+        public ActionResult SendNotification(string type, string notification)
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<NotificationsHub>();
+            hubContext.Clients.All.receiveNotification(type, notification);
+            return this.Content("Notification sent.<br />");
+        }
+    }
+}
---
+++ Source/Web/OnlineAtelier.Web/Hubs/NotificationsHub.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineAtelier.Web.Hubs
+{
+    using Microsoft.AspNet.SignalR;
+    using Microsoft.AspNet.SignalR.Hubs;
+
+    [HubName("notifications")]
+    public class NotificationsHub : Hub
+    {
+        public void SendNotification(string type, string notification)
+        {
+            this.Clients.Others.receiveNotification(type, notification);
+        }
+    }
+}
